9df4351bff2b184e00d469e7e3b89d9caea09b14
archiesoft <archiesoft@gmail.com>
2021-12-29 20:28:01 -0500

    port changed to 3000

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import postRoutes from './routes/posts.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.use(express.json({limit:"30mb", extended: true}));
 app.use(express.json({limit:"30mb", extended: true}));
 app.use(cors());
