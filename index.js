4d5fd216e290946f63bdef39f98b0a7bafc0c8bf
archiesoft <archiesoft@gmail.com>
2023-02-16 18:45:23 -0500

    initial commit

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json({limit:"30mb", extended: true}));
-app.use(express.json({limit:"30mb", extended: true}));
 app.use(cors());
 
 app.get('/', (req, res)=>{
