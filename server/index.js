1dd2695b4945e0cab52f3a8bcf0757014690b8d7
archiesoft <archiesoft@gmail.com>
2023-02-17 14:58:03 -0500

    adding server files and removing server .gitignore

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+//routes
+import postRoutes from './routes/posts.js';
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(express.json({limit:"30mb", extended: true}));
+app.use(cors());
+
+app.get('/', (req, res)=>{
+    res.send("Welcome to Memories Application")
+})
+
+app.use('/posts', postRoutes);
+
+
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser:true, useUnifiedTopology: true}) //not required but to avoid errors
+        .then(()=>app.listen(PORT, "0.0.0.0", ()=>{console.log(`Server running and connected to mongo on PORT: ${PORT}`)}))
+        .catch((error)=>console.log(error));
+//mongoose.set('useFindAndModify', false); //not supported in new version
